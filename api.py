d1860a2076d6706a7c236b102fca3e00afbef0bb
Sam <s.e.llinasramos@student.utwente.nl>
2026-05-28 12:31:42 +0200

    almost final

--- api.py
+++ api.py
@@ -1,12 +1,32 @@
+import threading
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from aircraft_store import (
     get_all_aircraft,
     get_aircraft
 )
 
+from receiver import receive_data
 from predict import estimate_fall
 
 app = FastAPI()
+
+# Allow frontend requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Start websocket receiver in background
+threading.Thread(
+    target=receive_data,
+    daemon=True
+).start()
 
 @app.get("/planes")
 def planes():
