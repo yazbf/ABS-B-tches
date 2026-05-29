479af806c142aba483bdedbeb9cffd27fba4ee21
yazbf <113351285+yazbf@users.noreply.github.com>
2026-05-29 18:54:05 +0200

    yeah

--- main.py
+++ main.py
@@ -10,3 +10,4 @@ thread.start()
 thread.join() #Wait for the receiver thread to finish (it won't, but this keeps the main thread alive)
 
 # API is launched separately with uvicorn
+#use uvicorn api:app --reload
